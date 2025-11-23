9f71d0dee28094c69d1853412f1282827c026dc8
FrostSue <emirhan_58600@hotmail.com>
2025-11-23 23:15:01 +0300

    Fixed env requirement

--- core/env_loader.py
+++ core/env_loader.py
@@ -10,8 +10,7 @@ class Config:
         TG_API_HASH = os.getenv("TELEGRAM_API_HASH")
         TG_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
         TG_CHANNEL_ID = int(os.getenv("TELEGRAM_CHANNEL_ID"))
-        LOG_CHANNEL_ID = int(os.getenv("TELEGRAM_LOG_CHANNEL_ID")) if os.getenv("TELEGRAM_LOG_CHANNEL_ID") else None        
-        ADMIN_ID = int(os.getenv("ADMIN_USER_ID")) if os.getenv("ADMIN_USER_ID") else None
+        LOG_CHANNEL_ID = int(os.getenv("TELEGRAM_LOG_CHANNEL_ID")) if os.getenv("TELEGRAM_LOG_CHANNEL_ID") else None
 
         TW_API_KEY = os.getenv("TWITTER_API_KEY")
         TW_API_SECRET = os.getenv("TWITTER_API_SECRET")
